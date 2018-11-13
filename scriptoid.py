32c55e883854aab91a94706c5633d1f09cf93e8f
dhsavell <dhsavell05@gmail.com>
2018-11-13 16:25:51 -0500

    Base script on SCRIPTOID_HOME envvar (closes #2)

--- scriptoid.py
+++ scriptoid.py
@@ -10,7 +10,6 @@ import toml
 from colorama import Fore, Style
 from semver import VersionInfo
 
-SCRIPTOID_HOME = os.environ['SCRIPTOID_HOME']
 SCRIPTOIDS_VERSION = VersionInfo(0, 1, 0)
 SCRIPT_CONFIG_FILENAME = 'script_info.toml'
 SCRIPT_CONFIG_REQUIRED_FIELDS = ['name', 'entry']
@@ -43,6 +42,12 @@ def die(msg, exit_code=1):
     exit(exit_code)
 
 
+try:
+    SCRIPTOID_HOME = os.environ['SCRIPTOID_HOME']
+except KeyError:
+    die('No value is set for environment variable SCRIPTOID_HOME. A suggested path is $HOME/scriptoids.')
+
+
 def directory_contains_scriptoid(directory):
     """
     Determines whether or not the given directory contains a scriptoid.
@@ -63,6 +68,23 @@ def script_definition_is_valid(script_definition):
            FILENAME_VALIDATION_REGEX.match(script_definition['script']['name'])
 
 
+def get_scriptoid_directory(scriptoid_name):
+    """
+    Gets the expected directory housing a given scriptoid.
+    :param scriptoid_name: Name of the scriptoid to get the directory for.
+    :return: Expected directory for the given scriptoid.
+    """
+    return os.path.join(SCRIPTOID_HOME, scriptoid_name)
+
+
+def get_bin_directory():
+    """
+    Gets the expected `bin` directory within the scriptoid home path.
+    :return: Path to the scriptoid `bin` directory.
+    """
+    return os.path.join(SCRIPTOID_HOME, 'bin')
+
+
 @click.group()
 def cli():
     pass
@@ -75,16 +97,17 @@ def new(name):
     Scaffolds a new scriptoid.
     """
     info(f'Creating new scriptoid {name}...')
+    scriptoid_path = get_scriptoid_directory(name)
 
     try:
-        os.mkdir(name)
+        os.makedirs(scriptoid_path, exist_ok=True)
     except FileExistsError:
-        die(f'A directory named {name} already exists.')
+        die(f'A scriptoid named {name} already exists.')
 
-    with open(os.path.join(name, f'{name}.sh'), 'w') as script:
+    with open(os.path.join(scriptoid_path, f'{name}.sh'), 'w') as script:
         script.write('#!/bin/bash')
 
-    with open(os.path.join(name, 'script_info.toml'), 'w') as metadata:
+    with open(os.path.join(scriptoid_path, 'script_info.toml'), 'w') as metadata:
         toml.dump({
             'script': {
                 'name': name,
@@ -95,7 +118,7 @@ def new(name):
             },
         }, metadata)
 
-    success(f'Created new scriptoid in {name}/.')
+    success(f'Created new scriptoid {name} in {scriptoid_path}.')
 
 
 @click.command()
@@ -104,16 +127,18 @@ def link(name):
     """
     Adds a link from a given scriptoid to the `bin` directory.
     """
-    info(f'Linking script {name} to bin/...')
+    info(f'Linking scriptoid {name}...')
+    bin_directory = get_bin_directory()
+    scriptoid_directory = get_scriptoid_directory(name)
 
-    if not os.path.isdir('bin'):
-        info('Directory bin/ does not exist, creating...')
-        os.mkdir('bin')
+    if not os.path.isdir(bin_directory):
+        info(f'Directory {bin_directory} does not exist, creating...')
+        os.makedirs(bin_directory, exist_ok=True)
 
-    if not directory_contains_scriptoid(name):
-        die(f'{name} does not appear to be a valid script directory.')
+    if not directory_contains_scriptoid(scriptoid_directory):
+        die(f'No scriptoid `{name}` found in directory {scriptoid_directory}.')
 
-    full_script_definition = toml.load(open(os.path.join(name, 'script_info.toml'), 'r'))
+    full_script_definition = toml.load(open(os.path.join(scriptoid_directory, 'script_info.toml'), 'r'))
 
     if not script_definition_is_valid(full_script_definition):
         die(f"{name}'s {SCRIPT_CONFIG_FILENAME} file is not valid.")
@@ -121,11 +146,10 @@ def link(name):
     script_info = full_script_definition['script']
     entry = script_info['entry']
     dest_name = script_info['name']
-
-    dest_path = os.path.abspath(os.path.join('bin', dest_name))
+    dest_path = os.path.abspath(os.path.join(bin_directory, dest_name))
 
     info('Creating symlink...')
-    os.symlink(os.path.abspath(os.path.join(name, entry)), dest_path)
+    os.symlink(os.path.abspath(os.path.join(scriptoid_directory, entry)), dest_path)
 
     info('Setting executable...')
     link_st = os.stat(dest_path)
@@ -141,16 +165,20 @@ def unlink(name):
     Removes a link for a given scriptoid from the `bin` directory.
     """
 
-    info(f'Unlinking {name} from bin/...')
+    info(f'Unlinking scriptoid {name}...')
 
-    if not os.path.isdir('bin'):
+    bin_directory = get_bin_directory()
+
+    if not os.path.isdir(bin_directory):
         die('No scripts are currently linked.')
 
-    if not os.path.islink(os.path.join('bin', name)):
-        die(f'Script {name} does not appear to be linked.')
+    link_path = os.path.join(bin_directory, name)
+
+    if not os.path.islink(link_path):
+        die(f'Script `{name}` does not appear to be linked.')
 
     info('Removing link...')
-    os.remove(os.path.join('bin', name))
+    os.remove(link_path)
 
     success(f'Unlinked scriptoid {name}.')
 
@@ -163,7 +191,4 @@ for command in [
     cli.add_command(command)
 
 if __name__ == '__main__':
-    if not SCRIPTOID_HOME:
-        die('No value is set for environment variable SCRIPTOID_HOME. A suggested path is $HOME/scriptoids.')
-
     cli()
