8523929750f9fb6de384bb245dee35e36c97cd6c
dhsavell <dhsavell05@gmail.com>
2018-11-13 15:59:40 -0500

    Begin reworking script to use environment variables

--- scriptoid.py
+++ scriptoid.py
@@ -8,33 +8,59 @@ import sys
 import click
 import toml
 from colorama import Fore, Style
+from semver import VersionInfo
 
+SCRIPTOID_HOME = os.environ['SCRIPTOID_HOME']
+SCRIPTOIDS_VERSION = VersionInfo(0, 1, 0)
 SCRIPT_CONFIG_FILENAME = 'script_info.toml'
 SCRIPT_CONFIG_REQUIRED_FIELDS = ['name', 'entry']
 FILENAME_VALIDATION_REGEX = re.compile(r'^[\w\-. ]+$')
 
 
 def info(msg):
+    """
+    Prints an informational message.
+    :param msg: Message to display.
+    """
     print(f'{Style.BRIGHT}{Fore.BLACK}  {msg}{Style.RESET_ALL}')
 
 
 def success(msg):
+    """
+    Prints a message indicating success.
+    :param msg: Message to display.
+    """
     print(f'{Style.BRIGHT}{Fore.GREEN}✔ {Fore.BLACK}{msg}{Style.RESET_ALL}')
 
 
-def die(msg):
+def die(msg, exit_code=1):
+    """
+    Prints a message denoting a failure and exits with a given exit code.
+    :param msg: Message to display.
+    :param exit_code: Code to return when exiting.
+    """
     sys.stderr.write(f'{Style.BRIGHT}{Fore.RED}✘ An error occurred: {Fore.BLACK}{msg}{Style.RESET_ALL}\n')
-    exit(1)
+    exit(exit_code)
 
 
-def is_script_directory(directory):
+def directory_contains_scriptoid(directory):
+    """
+    Determines whether or not the given directory contains a scriptoid.
+    :param directory: Directory to validate.
+    :return: Whether or not the given directory contains a scriptoid.
+    """
     return os.path.exists(os.path.join(directory, SCRIPT_CONFIG_FILENAME))
 
 
-def is_script_info_valid(script_info):
-    return 'script' in script_info and \
-           all(field in script_info['script'] for field in SCRIPT_CONFIG_REQUIRED_FIELDS) and \
-           FILENAME_VALIDATION_REGEX.match(script_info['script']['name'])
+def script_definition_is_valid(script_definition):
+    """
+    Determines whether or not a given script definition contains the necessary fields.
+    :param script_definition: Script definition, as a dictionary, to validate.
+    :return: Whether or not the given script definition is valid as of the current version.
+    """
+    return 'script' in script_definition and \
+           all(field in script_definition['script'] for field in SCRIPT_CONFIG_REQUIRED_FIELDS) and \
+           FILENAME_VALIDATION_REGEX.match(script_definition['script']['name'])
 
 
 @click.group()
@@ -46,7 +72,7 @@ def cli():
 @click.argument('name')
 def new(name):
     """
-    Scaffolds a new scriptoid
+    Scaffolds a new scriptoid.
     """
     info(f'Creating new scriptoid {name}...')
 
@@ -63,9 +89,9 @@ def new(name):
             'script': {
                 'name': name,
                 'entry': f'{name}.sh',
-                'description': '',          # TODO: Use when displaying info
-                'script_dependencies': [],  # TODO: Validate dependencies when linking
-                'path_dependencies': []     # TODO: Enforce programs in PATH when linking
+                'description': '',
+                'script_dependencies': [],
+                'path_dependencies': []
             },
         }, metadata)
 
@@ -76,7 +102,7 @@ def new(name):
 @click.argument('name')
 def link(name):
     """
-    Creates a symbolic link from a scriptoid to ./bin/
+    Adds a link from a given scriptoid to the `bin` directory.
     """
     info(f'Linking script {name} to bin/...')
 
@@ -84,15 +110,15 @@ def link(name):
         info('Directory bin/ does not exist, creating...')
         os.mkdir('bin')
 
-    if not is_script_directory(name):
+    if not directory_contains_scriptoid(name):
         die(f'{name} does not appear to be a valid script directory.')
 
-    script_info_toml = toml.load(open(os.path.join(name, 'script_info.toml'), 'r'))
+    full_script_definition = toml.load(open(os.path.join(name, 'script_info.toml'), 'r'))
 
-    if not is_script_info_valid(script_info_toml):
+    if not script_definition_is_valid(full_script_definition):
         die(f"{name}'s {SCRIPT_CONFIG_FILENAME} file is not valid.")
 
-    script_info = script_info_toml['script']
+    script_info = full_script_definition['script']
     entry = script_info['entry']
     dest_name = script_info['name']
 
@@ -105,15 +131,16 @@ def link(name):
     link_st = os.stat(dest_path)
     os.chmod(dest_path, link_st.st_mode | stat.S_IEXEC)
 
-    success(f'Linked {name} to bin/.')
+    success(f'Linked scriptoid {name}.')
 
 
 @click.command()
 @click.argument('name')
 def unlink(name):
     """
-    Removes a symbolic link from a scriptoid to ./bin/
+    Removes a link for a given scriptoid from the `bin` directory.
     """
+
     info(f'Unlinking {name} from bin/...')
 
     if not os.path.isdir('bin'):
@@ -125,7 +152,7 @@ def unlink(name):
     info('Removing link...')
     os.remove(os.path.join('bin', name))
 
-    success(f'Removed link to {name}')
+    success(f'Unlinked scriptoid {name}.')
 
 
 for command in [
@@ -136,4 +163,7 @@ for command in [
     cli.add_command(command)
 
 if __name__ == '__main__':
+    if not SCRIPTOID_HOME:
+        die('No value is set for environment variable SCRIPTOID_HOME. A suggested path is $HOME/scriptoids.')
+
     cli()
